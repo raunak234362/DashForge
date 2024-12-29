a99f242ad2a8e31421a1cef55905b937b3e8f7f6
Raunak <raunakdob@gmail.com>
2024-12-29 11:41:22 +0530

    refactor Service class; improve error handling and clean up code formatting

--- src/config/Service.js
+++ src/config/Service.js
@@ -22,8 +22,7 @@ class Service {
       const response = await axios.get(`${baseUrl}/files`);
       console.log("Response Data:", response.data);
       return response.data.files;
-    }
-    catch (error) {
+    } catch (error) {
       console.error("Error fetching all CSV data", error);
       return null;
     }
@@ -32,7 +31,7 @@ class Service {
   static async fetchCSVData(CSVId) {
     console.log("ID:", CSVId);
     try {
-    const response = await axios.get(`${baseUrl}/data/${CSVId}`);
+      const response = await axios.get(`${baseUrl}/data/${CSVId}`);
       console.log("Response Data:", response.data);
       return response.data.data;
     } catch (error) {
@@ -41,39 +40,27 @@ class Service {
     }
   }
 
-
   static async csvUpload({ csv_file }) {
-    // Check if the file is provided
     const file = csv_file[0];
     if (!file) {
-      console.error("No file selected");
       return;
     }
-    console.log("File details:", file);
-
     try {
       const formData = new FormData();
       // Append the file to the formData
       formData.append("file", file, file.name);
-      console.log("FormData created:", formData);
-
-      // Use the correct endpoint to upload the file
       const response = await fetch(`${baseUrl}/upload-csv`, {
         method: "POST",
         body: formData, // Send the formData with the file
-        // Do NOT manually set the 'Content-Type', as FormData will handle it
       });
 
-      // Log the response for debugging
       console.log("Response object:", response);
-
       if (!response.ok) {
         // Handle unsuccessful responses
         console.error(`HTTP error! Status: ${response.status}`);
         return;
       }
 
-      // Handle the response data (could be JSON, text, or any format depending on the backend)
       const responseData = await response.json(); // Assuming the server responds with JSON
       console.log("Response Data:", responseData);
 
